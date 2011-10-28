2bc2ea24d9c1db884db5dffd091f360e20d34096
Peter Cisewski <peter.cisewski@gmail.com>
2011-10-28 15:22:06 -0500

    Initial Commit.
    Signed-off-by: Peter Cisewski <peter.cisewski@gmail.com>

---
+++ MatchIt/MatchIt/App.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Expression.Blend.SampleData.SampleDataSource;
+using System;
+using System.Collections.Generic;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+
+namespace MatchIt
+{
+    partial class App
+    {
+        // TODO: Create a data model appropriate for your problem domain to replace the sample data
+        private static SampleDataSource _sampleData;
+
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            ShowGroupedCollection();
+            Window.Current.Activate();
+        }
+
+        public static void ShowGroupedCollection()
+        {
+            var page = new GroupedCollectionPage();
+            if (_sampleData == null) _sampleData = new SampleDataSource(page.BaseUri);
+            page.Groups = _sampleData.GroupedCollections;
+            Window.Current.Content = page;
+        }
+
+        public static void ShowCollectionSummary(IEnumerable<object> collection)
+        {
+            var page = new CollectionSummaryPage();
+            if (_sampleData == null) _sampleData = new SampleDataSource(page.BaseUri);
+            page.Items = collection;
+            page.Item = collection;
+            Window.Current.Content = page;
+        }
+
+        public static void ShowDetail(IEnumerable<object> collection, object item)
+        {
+            var page = new DetailPage();
+            if (_sampleData == null) _sampleData = new SampleDataSource(page.BaseUri);
+            page.Items = collection;
+            page.Item = item;
+            Window.Current.Content = page;
+        }
+    }
+}
